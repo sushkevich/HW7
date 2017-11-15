e2ba31f3daf93728147714df85a6c0febbbf569f
Slavik <sushkevich.v.p@gmail.com>
2017-11-15 15:32:12 +0300

    Initial commit

---
+++ a.js
@@ -0,0 +1,24 @@
+(function () {
+
+
+    document.getElementById('gallery').addEventListener('click', function (event) {
+        if (event.target.tagName === 'IMG') {
+        let imgUrl = event.target.currentSrc.split('/');
+        let tag = document.createElement('img');
+        tag.src = 'image/large/' + imgUrl[imgUrl.length - 1];
+        tag.id = 'anim';
+
+        document.getElementById('modal').appendChild(tag);
+        document.getElementById('modal').style.zIndex = '20';
+            document.getElementById('modal').style.opacity = '1';
+        }
+    });
+
+    document.getElementById('modal').addEventListener('click', function () {
+        let tag = document.querySelector('#modal img');
+        document.getElementById('modal').removeChild(tag);
+        document.getElementById('modal').style.zIndex = '-10';
+        document.getElementById('modal').style.opacity = '0';
+    })
+
+}());
